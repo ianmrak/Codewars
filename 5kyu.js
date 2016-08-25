f5d790b6895a577f6dcd80fe648f8f4a106c41f4
Ian Mrak <ianmrak@gmail.com>
2016-08-24 21:42:08 -0700

    Pete, the baker.

--- 5kyu.js
+++ 5kyu.js
@@ -9,3 +9,24 @@ var moveZeros = function (arr) {
   });
   return [...nums, ...zeros];
 }
+
+// Pete likes to bake some cakes. He has some recipes and ingredients. Unfortunately he is not good in maths. Can you help him to find out, how many cakes he could bake considering his recipes?
+
+function cakes(recipe, available) {
+  let totals = {};
+  for (let item in recipe) {
+    if (!available[item]) { return 0; }
+    let total = available[item];
+    while(total > 0) {
+      if (!totals[item]) { totals[item] = 0; }
+      if (total - recipe[item] >= 0) {
+        totals[item]++;
+      }
+      total -= recipe[item];
+    }
+  }
+  return totals[Object.keys(totals).reduce((a,b) => {
+    a = totals[a] > totals[b] ? b : a;
+    return a;
+  })];
+}
